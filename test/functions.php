f857fc945f7ca228dff12a9907b16344387016fc
Rosa <rosa@droogers.nl>
2021-08-04 16:33:48 +0100

    changed tiny bits

--- test/functions.php
+++ test/functions.php
@@ -13,18 +13,18 @@ class Functions extends TestCase
             'common_name' => 'Jewel Bug',
             'species' => 'Scutiphora Pedicellata',
             'date_spotted' => '2018-03-15',
-            'location_spotted' => 'India',
+            'country_spotted' => 'India',
             'size' => '5-20 mm',
             'image_path' => 'images/jewel_bug.jpeg'];
         $expectedOutput = '<div class="insects"><img src="images/jewel_bug.jpeg" alt="Image of a Jewel Bug"><h2>Jewel Bug</h2>' .
             '<p class="species">Scutiphora Pedicellata</p><p>Date spotted: 2018-03-15</p>' .
-             '<p>Location spotted: India</p><p>Size: 5-20 mm</p></div>';
+             '<p>Country spotted: India</p><p>Size: 5-20 mm</p></div>';
         $actualOutput = printInsect($input);
         $this->assertEquals($expectedOutput, $actualOutput);
     }
     public function testFailurePrintInsect()
     {
-        $input = ['id' => '1', 'name' => 'Jewel Bug', 'species' => 'Scutiphora Pedicellata', 'date_spotted' => '2018-03-15', 'location_spotted' => 'India', 'size' => '5-20 mm', 'image_path' => 'images/jewel_bug.jpeg'];
+        $input = ['id' => '1', 'name' => 'Jewel Bug', 'species' => 'Scutiphora Pedicellata', 'date_spotted' => '2018-03-15', 'country_spotted' => 'India', 'size' => '5-20 mm', 'image_path' => 'images/jewel_bug.jpeg'];
         $expectedOutput = 'I\'m sorry, that array doesn\'t contain the expected keys.';
         $actualOutput = printInsect($input);
         $this->assertEquals($expectedOutput, $actualOutput);
