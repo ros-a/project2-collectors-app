af31185f09973d068b47d453a4efe9fc5c7cece7
Rosa <rosa@droogers.nl>
2021-08-06 11:30:39 +0100

    changed smth

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function printInsect (array $dbRow): string {
-    if (array_key_exists('image_path', $dbRow) && array_key_exists('common_name', $dbRow) && array_key_exists('species', $dbRow) && array_key_exists('date_spotted', $dbRow) && array_key_exists('location_spotted', $dbRow) && array_key_exists('size', $dbRow)) {
+    if (array_key_exists('image_path', $dbRow) && array_key_exists('common_name', $dbRow) && array_key_exists('species', $dbRow) && array_key_exists('date_spotted', $dbRow) && array_key_exists('country_spotted', $dbRow) && array_key_exists('size', $dbRow)) {
         return '<div class="insects">' .
                     '<img src="' . $dbRow['image_path'] . '" alt="Image of a ' . $dbRow['common_name'] . '">' .
                     '<h2>' . $dbRow['common_name'] . '</h2>' .
